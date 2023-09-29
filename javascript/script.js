bc3572b2bff72b3fa51c312dc767180247ed755d
santino giatti <santinogiatti05@gmail.com>
2023-09-29 02:14:23 -0300

    2do commit

--- javascript/script.js
+++ javascript/script.js
@@ -3,25 +3,41 @@
 
   /* formulario pop up */
 
-document.addEventListener("DOMContentLoaded", function () {
-  const abrirFormularioBtn = document.getElementById("abrirFormulario");
-  const formularioContainer = document.getElementById("formularioContainer");
-  const body = document.body; // Obtener la referencia al body
+  document.addEventListener("DOMContentLoaded", function () {
+    const abrirFormularioBtn = document.getElementById("abrirFormulario");
+    const formularioContainer = document.getElementById("formularioContainer");
+    const body = document.body;
+  
+    // Mostrar el formulario pop-up cuando se hace clic en el botón
+    abrirFormularioBtn.addEventListener("click", function() {
+      formularioContainer.style.display = "flex";
+  
+      // Agregar la clase para la animación de fade-in
+      formularioContainer.classList.add("fadeIn"); // Aquí se agrega la clase
+  
+      body.classList.add("no-scroll");
+    });
+  
+    // Ocultar el formulario pop-up cuando se hace clic fuera de él
+    formularioContainer.addEventListener("click", function(event) {
+      if (event.target === formularioContainer) {
+        formularioContainer.style.display = "none";
+        body.classList.remove("no-scroll");
+      }
+    });
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+  const contentElements = document.querySelectorAll(".acordeon-content");
 
-  // Mostrar el formulario pop-up cuando se hace clic en el botón
-  abrirFormularioBtn.addEventListener("click", function() {
-    formularioContainer.style.display = "flex";
-    body.classList.add("no-scroll"); // Agregar la clase para deshabilitar el scroll
+  checkboxes.forEach(function (checkbox, index) {
+    checkbox.addEventListener("change", function () {
+      if (checkbox.checked) {
+        contentElements[index].style.maxHeight = contentElements[index].scrollHeight + "px";
+      } else {
+        contentElements[index].style.maxHeight = "0";
+      }
+    });
   });
-
-  // Ocultar el formulario pop-up cuando se hace clic fuera de él
-  formularioContainer.addEventListener("click", function(event) {
-    if (event.target === formularioContainer) {
-      formularioContainer.style.display = "none";
-      body.classList.remove("no-scroll"); // Quitar la clase para habilitar el scroll
-    }
   });
-});
 
 
 
